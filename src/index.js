09b9116459db37810930cec07fd0acbd538a91f6
arvindn97 <arvindbharathy.natarajan97@gmail.com>
2025-04-09 16:27:12 +1000

    fetching API Key from parameter store

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const { SSMClient, GetParameterCommand } = require("@aws-sdk/client-ssm");
+const ssmClient = new SSMClient();
+const ssmParameterPath = process.env.ssmParameterPath;
 //Main Handler Function
 exports.handler = async (event) => {
   const invocationPath = event.path;
@@ -8,11 +11,21 @@ exports.handler = async (event) => {
       //Current Price Info API
       case '/currentPriceInfo':
         if (event.httpMethod == 'POST') {
-            console.log("eventBody:",eventBody);
-            return apiResponse(200,{"message":"Success"});
+          //Payload validation
+          if (!eventBody.crypto || !eventBody.emailID || typeof eventBody.crypto !== 'string' || typeof eventBody.emailID !== 'string') {
+            return apiResponse(400, { "message": "Invalid request body" })
+          } else {
+            try {
+              let apiKey = await fetchAPIKey(ssmParameterPath); //retrieves apiKey from ssm parameter store
+              console.log("API Key:",apiKey);
+            } catch (error) {
+              console.error(error);
+              return apiResponse(error.statusCode, error.message);
+            }
+          }
         }
         else {
-
+          return apiResponse(405, { "message": `${event.httpMethod} is not allowed. Allowed Methods: POST` })
         }
         break;
       //Get Search History API
@@ -32,6 +45,21 @@ exports.handler = async (event) => {
     console.error(error);
   }
 };
+
+//function to fetch codeGecko API Key from SSM Parameter Store
+async function fetchAPIKey(key) {
+  try {
+    const params = {
+      Name: key
+    };
+    const command = new GetParameterCommand(params);
+    const response = await ssmClient.send(command); //retrieves ssm parameter details
+    return response.Parameter.Value;
+  } catch (error) {
+    console.error(error);
+    throw { statusCode: 502, message: "Failed to fetch API Key" }
+  }
+}
 
 //function to structure response to APIGW
 const apiResponse = (statusCode, body) => {
