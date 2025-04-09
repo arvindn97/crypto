3c1ef0c6599d9768f4e74ae02e84ab5860e2a437
arvindn97 <arvindbharathy.natarajan97@gmail.com>
2025-04-09 16:05:59 +1000

    added switch case for route in handler

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-exports.handler = async (event)=>{
-    console.log("Hello World");
+//Main Handler Function
+exports.handler = async (event) => {
+  const invocationPath = event.path;
+  const eventBody = JSON.parse(event.body);
+  try {
+    // determine which API endpoint is being invoked and route accordingly
+    switch (invocationPath) {
+      //Current Price Info API
+      case '/currentPriceInfo':
+        if (event.httpMethod == 'POST') {
+            console.log("eventBody:",eventBody);
+        }
+        else {
+
+        }
+        break;
+      //Get Search History API
+      case '/getSearchHistory':
+        if (event.httpMethod == 'POST') {
+            console.log("eventBody:",eventBody)
+        }
+        else {
+
+        }
+        break;
+      // handle unsupported paths
+      default:
+        break;
+    }
+  } catch (error) {
+    console.error(error);
+  }
 };
