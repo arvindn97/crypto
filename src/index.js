cce2d8e3bd56e7ed481178792e9a895e591a0f0c
arvindn97 <arvindbharathy.natarajan97@gmail.com>
2025-04-10 11:11:24 +1000

    updated code to handle multiple currencies

--- src/index.js
+++ src/index.js
@@ -23,24 +23,15 @@ exports.handler = async (event) => {
       case '/currentPriceInfo':
         if (event.httpMethod == 'POST') {
           //Payload validation
-          if (!eventBody.crypto || !eventBody.emailID || typeof eventBody.crypto !== 'string' || typeof eventBody.emailID !== 'string') {
+          if (!eventBody.crypto || !eventBody.emailID || typeof eventBody.crypto !== 'object' || typeof eventBody.emailID !== 'string' || eventBody.crypto.length == 0) {
             return apiResponse(400, { "message": "Invalid request body" })
           } else {
             try {
               let apiKey = await fetchAPIKey(ssmParameterPath); //retrieves apiKey from ssm parameter store
               let data = await fetchCryptoDetails(eventBody, apiKey); // retrieves crypto response from codeGecko API
               let dbInsertResult = await saveCryptoDetails(eventBody, data); //inserts the retrieved codeGecko API's response to DB
-              let emailData = await sendEmail(eventBody, data); // triggers mail to the user on the retrieved data
-              //Framing response object
-              let responseStructure = {
-                crypto: Object.keys(dbInsertResult.message)[0],
-                price: "A$ " + dbInsertResult.message[eventBody.crypto].aud,
-                marketCap: "A$ " + dbInsertResult.message[eventBody.crypto].aud_market_cap,
-                volume24h: "A$ " + dbInsertResult.message[eventBody.crypto].aud_24h_vol,
-                change24h: "A$ " + dbInsertResult.message[eventBody.crypto].aud_24h_change,
-                timestamp: moment().format("DD-MM-YYYY HH:mm:ss")
-              }
-              return apiResponse(dbInsertResult.statusCode, responseStructure);
+              let emailData = await sendEmail(eventBody, dbInsertResult); // triggers mail to the user on the retrieved data
+              return apiResponse(200, { data: emailData });
             } catch (error) {
               console.error(error);
               return apiResponse(error.statusCode, error.message);
@@ -60,7 +51,7 @@ exports.handler = async (event) => {
           } else {
             try {
               let data = await fetchSearchHistory(eventBody);  // retrieves user specific search history from DB
-              return apiResponse(data.statusCode, {"searchHistory":data.message});
+              return apiResponse(data.statusCode, { "searchHistory": data.message });
             } catch (error) {
               console.error(error);
               return apiResponse(error.statusCode, error.message);
@@ -99,10 +90,10 @@ async function fetchAPIKey(key) {
 
 //function to fetch crypto details from codeGecko API (third-party API)
 async function fetchCryptoDetails(eventBody, apiKey) {
-  //Currently supports only one crypto insertion. Needs iteration support for multiple coins.
-  let ids = eventBody.crypto.split(','); //for enabling multiple crypto request support in future if required
+  //iteration support for multiple coins.
+  let ids = eventBody.crypto;
   let queryParams = '?vs_currencies=aud&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true&precision=2&ids=';
-  queryParams += ids.join('%2C'); //for enabling multiple crypto to append in queryParam for future if required
+  queryParams += ids.join('%2C'); //for enabling multiple crypto to append in queryParam
   let url = apiEndpoint + queryParams;
   const options = {
     method: 'GET',
@@ -115,11 +106,7 @@ async function fetchCryptoDetails(eventBody, apiKey) {
     if (Object.keys(data).length == 0) {
       throw { statusCode: 400, message: `Cryptocurrency "${eventBody.crypto}" does not exist or is not supported.` } //custom error throw for non existing crypto
     } else {
-      let responseObj = {
-        "statusCode": 200,
-        "message": data
-      }
-      return responseObj;
+      return data;
     }
   } catch (error) {
     console.error(error);
@@ -135,23 +122,33 @@ async function fetchCryptoDetails(eventBody, apiKey) {
 //function to insert the crypto details received from codeGecko API to DynamoDB
 async function saveCryptoDetails(eventBody, data) {
   try {
-    // Currently supports only one crypto insertion. Needs iteration support for multiple coins.
-    const params = {
-      TableName: TableName,
-      Item: marshall({
+    //iteration support for multiple coins.
+    const cryptos = Object.keys(data);
+    let resultData = [];
+    for (let i = 0; i < cryptos.length; i++) {
+      const cryptocurrency = cryptos[i];
+      const cryptoData = data[cryptocurrency];
+      let cryptoObject = {
         "ID": uuidv4(),
-        "crypto": eventBody.crypto,
+        "crypto": cryptocurrency,
         "emailID": eventBody.emailID,
-        "price": "A$" + data.message[eventBody.crypto].aud,
-        "marketCap": data.message[eventBody.crypto].aud_market_cap,
-        "volume24h": data.message[eventBody.crypto].aud_24h_vol,
-        "change24h": data.message[eventBody.crypto].aud_24h_change,
+        "price": "A$" + cryptoData.aud,
+        "marketCap": "A$" + cryptoData.aud_market_cap,
+        "volume24h": "A$" +cryptoData.aud_24h_vol,
+        "change24h": "A$" +cryptoData.aud_24h_change,
         "timestamp": moment().format('YYYY-MM-DD HH:mm:ss')
-      })
-    };
-    const command = new PutItemCommand(params);
-    const response = await client.send(command);
-    return data;
+      }
+      const params = {
+        TableName: TableName,
+        Item: marshall(cryptoObject)
+      };
+      const command = new PutItemCommand(params);
+      const response = await client.send(command);
+      delete cryptoObject.ID;
+      delete cryptoObject.emailID;
+      resultData.push(cryptoObject);
+    }
+    return resultData;
   } catch (error) {
     console.error(error);
     throw { statusCode: 500, message: "Failed to input data to DB" }
@@ -163,24 +160,21 @@ async function sendEmail(eventBody, data) {
   // Currently supports only one crypto at a time. Needs loop for multiple coins.
   try {
     // declaring email body
-    const emailBodyText = `
-      Hi ${eventBody.emailID},
-
-      Here is your requested cryptocurrency price update:
-
-      Cryptocurrency: ${eventBody.crypto}
-      Current Price: A$ ${data.message[eventBody.crypto].aud}
-      Market Cap: A$ ${data.message[eventBody.crypto].aud_market_cap}
-      Volume (24h): A$ ${data.message[eventBody.crypto].aud_24h_vol}
-      Change (24h): A$ ${data.message[eventBody.crypto].aud_24h_change}
-
-      We hope this helps you stay on top of the market trends.
-
-      If you like to get more updates, feel free to come back and use our service anytime.
-
-      Warm regards,
-      Crypto Checkers  
-    `
+    let emailBodyContent = "";
+    const emailBodyHeader = ` Hi ${eventBody.emailID},\n\nHere is your requested cryptocurrency price update:\n\n`;
+    const emailBodyFooter = `We hope this helps you stay on top of the market trends.\n\nIf you like to get more updates, feel free to come back and use our service anytime.\n\nWarm regards,\nCrypto Checkers`
+    //iteration to handle multiple cryptoCurrency data
+    for (let i = 0; i < data.length; i++) {
+      emailBodyContent += `${data[i].crypto.toUpperCase()}:\n`;
+      emailBodyContent += `Price: ${data[i].price}\n`;
+      emailBodyContent += `Market Cap: ${data[i].marketCap.toLocaleString("en-AU")}\n`;
+      emailBodyContent += `24h Volume: ${data[i].volume24h.toLocaleString("en-AU")}\n`;
+      emailBodyContent += `24h Change: ${data[i].change24h}%\n`;
+      emailBodyContent += `Timestamp: ${data[i].timestamp}\n`;
+      emailBodyContent += `---------------------------\n`;
+    }
+    const emailBodyText = emailBodyHeader + emailBodyContent + emailBodyFooter;
+    console.log("EmailBody:", emailBodyText);
     const params = {
       Destination: {
         ToAddresses: [eventBody.emailID],
@@ -199,7 +193,7 @@ async function sendEmail(eventBody, data) {
     };
     const command = new SendEmailCommand(params);
     const response = await sesClient.send(command);
-    return response;
+    return data;
   } catch (error) {
     console.error(error);
     throw { statusCode: 500, message: "Failed to send email" }
